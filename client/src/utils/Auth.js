a7005943d52232611fc536556f27f25e5c04e135
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-22 19:40:47 -0400

    Added function to validate emails

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -1,4 +1,11 @@
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
+
+// function to validate emails for contact form
+export function validateEmail(email) {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 class AuthService {
   getProfile() {
@@ -24,22 +31,22 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
     // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
+    localStorage.setItem("id_token", idToken);
 
-    window.location.assign('/');
+    window.location.assign("/");
   }
 
   logout() {
     // Clear user token and profile data from localStorage
     // axios.defaults.headers.common["Authorization"] = null;
-    localStorage.removeItem('id_token');
+    localStorage.removeItem("id_token");
     // this will reload the page and reset the state of the application
-    window.location.assign('/');
+    window.location.assign("/");
   }
 }
 
