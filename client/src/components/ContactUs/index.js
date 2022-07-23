1704a2c1575bbe52031f2001afa6bf74abece6ab
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-23 00:23:04 -0400

    Add imports and Contact function

---
+++ client/src/components/ContactUs/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ContactForm from "../ContactForm/index.js";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import "../../App.css";
+
+function Contact()
