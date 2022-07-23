84a06bd0dab3d7381dd963a267e92ccafe94521d
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-23 00:30:43 -0400

    Added Get in Touch header

--- client/src/components/ContactUs/index.js
+++ client/src/components/ContactUs/index.js
@@ -6,4 +6,13 @@ import ContactForm from "../ContactForm/index.js";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import "../../App.css";
 
-function Contact()
+function Contact() {
+    return (
+        <div className="container Jumbotron p-5 mb-0">
+           <Container className="text-center">
+              <div className="p-md-5">
+                <h1>Get in Touch</h1>
+              </div>
+            </Container>
+          
+}
