a994bb95b8f74213a4a68cdbc63aee08a0e68ca2
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-23 00:33:15 -0400

    Added Send a Message to contact us page

--- client/src/components/ContactUs/index.js
+++ client/src/components/ContactUs/index.js
@@ -7,15 +7,15 @@ import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import "../../App.css";
 
 function Contact() {
-    return (
-        <div className="container Jumbotron p-5 mb-0">
-           <Container className="text-center">
-              <div className="p-md-5">
-                <h1>Get in Touch</h1>
-              </div>
-            </Container>
-          
-        <Container fluid className="shadow bg-light mb-5">
+  return (
+    <div className="container Jumbotron p-5 mb-0">
+      <Container className="text-center">
+        <div className="p-md-5">
+          <h1>Get in Touch</h1>
+        </div>
+      </Container>
+
+      <Container fluid className="shadow bg-light mb-5">
         <Container className="p-4">
           <Row>
             <Col>
@@ -29,6 +29,20 @@ function Contact() {
                     Email
                   </a>
                 </p>
-                </Container>
+              </Container>
             </Col>
+
+            <Col sm={12} md={6} lg={8} className="pt-sm-4 pt-lg-0">
+              <Container>
+                <h4 style={{ color: "#F06543" }}>Send a Message</h4>
+                <ContactForm />
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </div>
+  );
 }
+
+export default Contact;
