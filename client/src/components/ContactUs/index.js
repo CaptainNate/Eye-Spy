36b9a4eead9cddf6952e3d6aacff7580303a2f7b
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-23 00:54:25 -0400

    Removed unused icons

--- client/src/components/ContactUs/index.js
+++ client/src/components/ContactUs/index.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ContactForm from "../ContactForm/index.js";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 import "../../App.css";
 
 function Contact() {
