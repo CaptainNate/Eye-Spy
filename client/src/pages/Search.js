36fa6dc34ae874f459ea322cc4916e559535a499
maggiejoe <maggiejoseph94@gmail.com>
2022-07-23 13:34:40 -0400

    updated searchbar component and started styling search-page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import SearchBar from '../components/SearchBar';
+
 const Search = () => {
 
     // add conditional statements to generate proper search here
 
     return (
-        <main>
-            
+        <main className='container d-flex justify-content-center'>
+            <div className=''>
+            <div className='text-center m-5'>
+                <h2 className='mb-5'>What kind of adventure is next?</h2>
+                <h4 className='mb-4'>Please select the activity you're interested in</h4>
+            </div>
+            <div>
+                <SearchBar />
+            </div>
+            </div>
         </main>
     )
 }
