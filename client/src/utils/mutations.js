94c68ed4f50d9aaeb2308adbff0f9f18cf262b2e
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-24 02:33:00 -0400

    Update login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,5 +15,13 @@ export const ADD_USER = gql`
 
 // need to add mutations for LOGIN_USER with email and password
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {}
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
