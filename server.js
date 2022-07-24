736292eb477bcb27ff4f6651a134e443c4bf566b
Luiz Medina <luizmedina87@gmail.com>
2022-07-24 12:08:30 -0400

    initial models files

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("./routes"));
 
-mongoose.connect();
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+mongoose.set("debug", true);
 
-app.listen();
+app.listen(PORT, () =>
+  console.log(`You are now connected to localhost:${PORT}`)
+);
