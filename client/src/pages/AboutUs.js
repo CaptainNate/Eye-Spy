212c8c46e17545157447f91e7e5153ec01507e5e
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-24 15:52:07 -0400

    Update button positions

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -6,8 +6,12 @@ const AboutUs = () => {
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Euismod nisi porta lorem mollis aliquam ut porttitor leo. Augue neque gravida in fermentum. Sem viverra aliquet eget sit amet tellus cras. Imperdiet proin fermentum leo vel orci. Placerat in egestas erat imperdiet sed euismod. Cras adipiscing enim eu turpis egestas. Eget nunc scelerisque viverra mauris in aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Eget duis at tellus at urna condimentum mattis pellentesque id. Porta lorem mollis aliquam ut porttitor. Donec massa sapien faucibus et molestie ac feugiat sed. Vel risus commodo viverra maecenas accumsan. Dictum at tempor commodo ullamcorper a lacus vestibulum sed. Aliquet bibendum enim facilisis gravida neque convallis a cras. Enim eu turpis egestas pretium aenean pharetra magna.
             </p>
+            <div class="btn-lg">
             <a class="btn btn-primary" href="/contact-us" role="button">Questions? Contact Us!</a>
+            </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end btn-lg">
             <a class="btn btn-primary" href="/activity-search" role="button">Start Searching!</a>
+            </div>
         </div>
         </div>
 
