ae19f9f4830efc82700fc1747bd27b88ffb40020
Luiz Medina <luizmedina87@gmail.com>
2022-07-24 13:45:02 -0400

    Created User Schema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,93 @@ const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
   {
-    username: {},
-    email: {},
-    posts: {},
-    likes: {},
-    hates: {},
-    
-  },{});
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    hates: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    favorites: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    is_moderator: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// returns the number of comments a user has
+UserSchema.virtual("commentsCount").get(function () {
+  return this.comments.length;
+});
+
+// returns the number of likes a user has
+UserSchema.virtual("likesCount").get(function () {
+  return this.likes.length;
+});
+
+// returns the number of hates a user has
+UserSchema.virtual("hatesCount").get(function () {
+  return this.hates.length;
+});
+
+// returns the number of favorites a user has
+UserSchema.virtual("favoritesCount").get(function () {
+  return this.favorites.length;
+});
+
+// returns the number of posts a user has
+UserSchema.virtual("postsCount").get(function () {
+  return this.posts.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
