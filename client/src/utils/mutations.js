b43e451e52721466b5fb5f631bee90b90215c3a3
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-24 02:31:39 -0400

    Added addUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,17 @@
 import gql from "@apollo/client";
 
-// need to add mutations for ADD_USER
-// need to add mutations for LOGIN_USER
+// need to add mutations for ADD_USER with email and password parts
+export const ADD_USER = gql`
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// need to add mutations for LOGIN_USER with email and password
+export const LOGIN_USER = gql``;
