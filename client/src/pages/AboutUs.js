db18d39a438a8f7427a284835a81e05b02857abb
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-25 11:53:56 -0400

    Added styling to header and about us text

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,9 +1,9 @@
 const AboutUs = () => {
     return (
         <div class="text-center">
-            <h1>About Us</h1>
+            <h1 class="fs-1 fw-bold">About Us</h1>
         <div>
-            <p>
+            <p class="fst-italic">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Euismod nisi porta lorem mollis aliquam ut porttitor leo. Augue neque gravida in fermentum. Sem viverra aliquet eget sit amet tellus cras. Imperdiet proin fermentum leo vel orci. Placerat in egestas erat imperdiet sed euismod. Cras adipiscing enim eu turpis egestas. Eget nunc scelerisque viverra mauris in aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Eget duis at tellus at urna condimentum mattis pellentesque id. Porta lorem mollis aliquam ut porttitor. Donec massa sapien faucibus et molestie ac feugiat sed. Vel risus commodo viverra maecenas accumsan. Dictum at tempor commodo ullamcorper a lacus vestibulum sed. Aliquet bibendum enim facilisis gravida neque convallis a cras. Enim eu turpis egestas pretium aenean pharetra magna.
             </p>
             <div class="btn btn-primary btn-lg">
