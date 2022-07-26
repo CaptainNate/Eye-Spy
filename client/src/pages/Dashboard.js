52170c9ad3872a47beec8da78a876083be4130d3
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-26 01:57:06 -0400

    Center show more favorites button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,8 +4,15 @@ import Favorites from "../components/Favorites";
 function Dashboard() {
   return (
     <div>
-      <h1 class="text-center">Welcome User</h1>
-      <Favorites />
+      <div class="text-center">
+        <h1 class="text-center">Welcome User</h1>
+        <Favorites />
+        <br></br>
+        <br></br>
+        <button type="button" class="btn btn-primary">
+          Show More Favorites
+        </button>
+      </div>
     </div>
   );
 }
