62fb721136b79c38b54b8d0251dabbef902f1d5b
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-26 01:40:14 -0400

    Added welcome and favourites

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Favorites from "../components/Favorites";
 
 function Dashboard() {
-    return()
+  return (
+    <div>
+      <h1 class="text-center">Welcome User</h1>
+      <Favorites />
+    </div>
+  );
 }
+
+export default Dashboard;
