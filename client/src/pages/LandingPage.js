0b04df6bee4d66b15c8ecf1181d9ab6e99fd89fc
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-27 16:25:26 -0400

    Updated bootstrap cards for reviews

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -18,21 +18,24 @@ function LandingPage() {
                     <h2 className="">Welcome to Eye Spy...</h2>
                     <br />
                     <br />
-    <div class="container-fluid py-2">
+                    <p>hello</p>
+    <div class="container-fluid py-2 card mb-3 scrolling-wrapper">
     {/* <h2 class="font-weight-light"></h2> */}
-    <div class="d-flex flex-row flex-nowrap">
+    <div class="d-flex flex-row flex-nowrap row g-0">
+      
         <div class="card card-body"><p>Wow! I never thougth I would find a site like this. I was able to find so many cool and unique places that were so close to home!</p>
         <p> - Scott H.</p></div>
         <div class="card card-body"><p>At first I thought it was such a weird webpage but it ended up being exactly what I was looking for! I can't even recommend it enough to anyone who likes being outdoors.</p>
         <p>-Theodore M.</p></div>
         <div class="card card-body"><p>I never thought anyone would think of an idea to find these unique gems. I'm always searching for these and a lot of them are just not what I'm looking for. This hits the nail on the head.</p>
         <p>-Cleo P.</p></div>
-        <div class="card card-body"><p>Eye Spy is a name that indeed matches. It's definitely got an eye for rare spots. I thought I was good, but they're even better.</p>
-        <p>-John E.</p></div>
-        <div class="card card-body"><p>I was never one to look online to find places to explore, that weren't so busy, but here we are.I found this site and now I can't stop checking back!</p>
-        <p>-Cillia T.</p></div>
+        {/* <div class="card card-body"><p>Eye Spy is a name that indeed matches. It's definitely got an eye for rare spots. I thought I was good, but they're even better.</p>
+        <p>-John E.</p></div> */}
+        {/* <div class="card card-body"><p>I was never one to look online to find places to explore, that weren't so busy, but here we are.I found this site and now I can't stop checking back!</p>
+        <p>-Cillia T.</p></div> */}
         <div class="card card-body"><p>What a refreshing site. It matches what I was looking for but just the webpage was so pleasant and nice to look at. I wish I could keep looking. I love the feature being able to add our own suggestions onto the website too! I feel so engaged.</p>
         <p>-Lilliana L.</p></div>
+        
     </div>
 </div>
 
