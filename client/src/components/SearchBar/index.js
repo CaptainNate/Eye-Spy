a1e1fbf9511cb6243031f3bc8643526fb4b5c0ed
maggiejoe <maggiejoseph94@gmail.com>
2022-07-27 11:29:23 -0400

    added image upload to addDestination.js page

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -12,13 +12,13 @@ function SearchBar() {
                 <button className="all-btns p-2 rounded px-5 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                     Select an outdoor activity <span class="selected"></span>
                 </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" style={{width:"100%"}}>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" style={{width:"50%"}}>
                     <li><a id="search-bar-hiking" className="dropdown-item text-center" href="/">Hiking Trails</a></li>
                     <li><a id="search-bar-camping" className="dropdown-item text-center" href="/">Camping</a></li>
                     <li><a id="search-bar-biking" className="dropdown-item text-center" href="/">Mountain Biking</a></li>
                     <li><a id="search-bar-swimming" className="dropdown-item text-center" href="/">Swimming</a></li>
                     <li><a id="search-bar-water-sport" className="dropdown-item text-center" href="/">Water Sports</a></li>
-                    <li><a id="search-bar-view-all" className="dropdown-item text-center" href="/">View All</a></li>
+                    {/* <li><a id="search-bar-view-all" className="dropdown-item text-center" href="/">View All</a></li> */}
                 </ul>
             </div>
         </div>
--- client/src/pages/AddDestination.js
+++ client/src/pages/AddDestination.js
@@ -6,40 +6,74 @@ function AddDestination() {
     // add a conditional statement so that all items in add-destination can be saved on click
     // create a formhandler?
 
+    // Image Preview
+    const loadFile = function (event) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            const output = document.getElementById('output');
+            output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+
     return (
-        <div className="container d-flex justify-content-center">
-            <div className="col-6">
-                <div>
-                    <h2 className="text-center my-4 pb-4 border-bottom border-dark">Add Destination</h2>
+        <div>
+            <div className="container">
+                <div className="">
+                    <div>
+                        <h2 className="text-center my-4 pb-4 border-bottom border-dark">Add Destination</h2>
+                    </div>
+                    <form>
+                        <div className="d-flex">
+                            <div className="row">
+                                <div className="col-6">
+                                    <div>
+                                        <label className="form-label">Title</label>
+                                        <input id="destination-title" className="form-control" placeholder="Title" name="title" type="text"></input>
+                                    </div>
+                                    <div>
+                                        <label className="form-label">Address</label>
+                                        <input id="destination-address" className="form-control" placeholder="Address" name="address" type="text"></input>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="mt-4">
+                                        <label htmlFor="validationTextarea" className="form-label">Description</label>
+                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Please enter a brief description" name="description" type="text" rows="5" style={{ height: "100%" }} required></textarea>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div>
+                                        <h3 className="text-center my-4 pb-4 border-bottom border-dark">Please Select A Category</h3>
+                                        <SearchBar />
+                                    </div>
+                                </div>
+                                {/* upload image */}
+                                <div className="col-6 text-center">
+                                    <h3 className="text-center my-4 pb-4 border-bottom border-dark">Upload A Photo</h3>
+                                    <div className="text-center">
+                                        <label htmlFor="file" className="File"></label>
+                                        <input className="text-center" type="file" id="img-file" name="file" accept="image/*" required onChange={loadFile} />
+                                        <div className="col-12">
+                                            {/* warning bc react wants an alt in the img element (alt is not necessary) */}
+                                            <img id="output" className="m-3 " style={{ height: "250px" }} />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 text-center mt-4">
+                                    <button className="addDest-btn p-2 rounded" type="submit">Add Destination</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <form>
-                    <div>
-                        <label className="form-label">Title</label>
-                        <input id="destination-title" className="form-control" placeholder="Title" name="title" type="text"></input>
-                    </div>
-                    <div>
-                        <label className="form-label">Address</label>
-                        <input id="destination-address" className="form-control" placeholder="Address" name="address" type="text"></input>
-                    </div>
-                    <div>
-                        <h3 className="text-center my-4 pb-4 border-bottom border-dark">Please Select A Category</h3>
-                        <SearchBar />
-                    </div>
-                    <div className="mt-4">
-                        <label for="validationTextarea" className="form-label">Description</label>
-                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Please enter a brief description" name="description" type="text" rows="10" style={{height:"100%"}} required></textarea>
-                    </div>
-
-                    {/* upload image content from Fernando to go in this div */}
-                    <div>
-
-                    </div>
-
-                    <div className="text-center mt-4">
-                        <button className="all-btns p-2 rounded" type="submit">Add Destination</button>
-                    </div>
-                </form>
+            < br />
+            < br />
+            < br />
             </div>
+            < br />
+            < br />
         </div>
     )
 }
