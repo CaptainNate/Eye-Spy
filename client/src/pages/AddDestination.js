8f9dcf1080b555eb53f4197641b0e1bd264a0017
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-28 18:18:04 -0400

    change auth.js file name

--- client/src/pages/AddDestination.js
+++ client/src/pages/AddDestination.js
@@ -1,93 +1,137 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // component imports
-import AddDestSearchBar from '../components/AddDestSearchBar';
-import AddDestModal from '../components/SuccessModal';
+import AddDestSearchBar from "../components/AddDestSearchBar";
+import AddDestModal from "../components/SuccessModal";
 
 // bootstrap imports
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 function AddDestination() {
+  // useState variables for modals
+  const [modalShow, setModalShow] = React.useState(false);
 
-    // useState variables for modals
-    const [modalShow, setModalShow] = React.useState(false);
+  // add a conditional statement so that all items in add-destination can be saved on click
+  // create a formhandler?
 
-    // add a conditional statement so that all items in add-destination can be saved on click
-    // create a formhandler?
+  // Image Preview
+  const loadFile = function (event) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const output = document.getElementById("output");
+      output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
 
-    // Image Preview
-    const loadFile = function (event) {
-        const reader = new FileReader();
-        reader.onload = function () {
-            const output = document.getElementById('output');
-            output.src = reader.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    }
-
-    return (
-        <div>
-            <div className="container p-5 mb-5">
-                <div className="">
-                    <div>
-                        <h1 className="text-center mb-5 pb-3 border-bottom border-dark header-font">Add Destination</h1>
-                    </div>
-                    <form className="container d-flex justify-content-center">
-                        <div className="">
-                            <div className="row justify-content-center">
-                                <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
-                                    <div className="pb-2">
-                                        <label className="form-label">Title</label>
-                                        <input id="destination-title" className="form-control" placeholder="Title" name="title" type="text"></input>
-                                    </div>
-                                    <div className="">
-                                        <label className="form-label">Address</label>
-                                        <input id="destination-address" className="form-control" placeholder="Address" name="address" type="text"></input>
-                                    </div>
-                                </div>
-                                <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
-                                    <div className="">
-                                        <label htmlFor="validationTextarea" className="form-label">Description</label>
-                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Please enter a brief description" name="description" type="text" rows="5" style={{ height: "100%" }} required></textarea>
-                                    </div>
-                                </div>
-                                <div className="col-5 m-2 rounded shadow-lg floating-box-bg">
-                                    <div>
-                                        <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">Please Select A Category</h4>
-                                        <AddDestSearchBar />
-                                    </div>
-                                </div>
-                                {/* upload image */}
-                                <div className="col-5 m-2 text-center rounded shadow-lg floating-box-bg">
-                                    <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">Upload A Photo</h4>
-                                    <div className="text-center">
-                                        <label htmlFor="file" className="File"></label>
-                                        <input className="text-center" type="file" id="img-file" name="file" accept="image/*" required onChange={loadFile} />
-                                        <div className="col-12">
-                                            {/* warning bc react wants an alt in the img element (alt is not necessary) */}
-                                            <img id="output" className="m-3 " style={{ height: "250px" }} alt="..." />
-                                        </div>
-                                    </div>
-                                </div>
-
-                                {/* Open Modal */}
-                                <Button className="all-btns m-2 p-2 rounded text-center col-4" onClick={() => setModalShow(true)}>
-                                    Add Destination
-                                </Button>
-                                <AddDestModal
-                                    show={modalShow}
-                                    onHide={() => setModalShow(false)}
-                                ></AddDestModal>
-                            </div>
-                        </div>
-                    </form>
+  return (
+    <div>
+      <div className="container p-5 mb-5">
+        <div className="">
+          <div>
+            <h1 className="text-center mb-5 pb-3 border-bottom border-dark header-font">
+              Add Destination
+            </h1>
+          </div>
+          <form className="container d-flex justify-content-center">
+            <div className="">
+              <div className="row justify-content-center">
+                <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
+                  <div className="pb-2">
+                    <label className="form-label">Title</label>
+                    <input
+                      id="destination-title"
+                      className="form-control"
+                      placeholder="Title"
+                      name="title"
+                      type="text"
+                    ></input>
+                  </div>
+                  <div className="">
+                    <label className="form-label">Address</label>
+                    <input
+                      id="destination-address"
+                      className="form-control"
+                      placeholder="Address"
+                      name="address"
+                      type="text"
+                    ></input>
+                  </div>
                 </div>
-                < br />
-                < br />
-                < br />
+                <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
+                  <div className="">
+                    <label htmlFor="validationTextarea" className="form-label">
+                      Description
+                    </label>
+                    <textarea
+                      className="form-control is-invalid"
+                      id="validationTextarea"
+                      placeholder="Please enter a brief description"
+                      name="description"
+                      type="text"
+                      rows="5"
+                      style={{ height: "100%" }}
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="col-5 m-2 rounded shadow-lg floating-box-bg">
+                  <div>
+                    <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">
+                      Please Select A Category
+                    </h4>
+                    <AddDestSearchBar />
+                  </div>
+                </div>
+                {/* upload image */}
+                <div className="col-5 m-2 text-center rounded shadow-lg floating-box-bg">
+                  <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">
+                    Upload A Photo
+                  </h4>
+                  <div className="text-center">
+                    <label htmlFor="file" className="File"></label>
+                    <input
+                      className="text-center"
+                      type="file"
+                      id="img-file"
+                      name="file"
+                      accept="image/*"
+                      required
+                      onChange={loadFile}
+                    />
+                    <div className="col-12">
+                      {/* warning bc react wants an alt in the img element (alt is not necessary) */}
+                      <img
+                        id="output"
+                        className="m-3 "
+                        style={{ height: "250px" }}
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                {/* Open Modal */}
+                <Button
+                  className="all-btns m-2 p-2 rounded text-center col-4"
+                  onClick={() => setModalShow(true)}
+                >
+                  Add Destination
+                </Button>
+                <AddDestModal
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                ></AddDestModal>
+              </div>
             </div>
+          </form>
         </div>
-    )
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default AddDestination;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -18,21 +18,32 @@ function Dashboard(props) {
     // placeholder cards that will be filled with data from the backend
 
     <div className="container p-5 mb-5">
-                  <div className="">
-                <h1 className="mb-5 pb-3 border-bottom border-dark header-font text-center">Your Dashboard</h1>
-            </div>
+      <div className="">
+        <h1 className="mb-5 pb-3 border-bottom border-dark header-font text-center">
+          Your Dashboard
+        </h1>
+      </div>
       <div className="d-flex row justify-content-center text-center">
         <div className="col-5 p-4 m-2 shadow-lg floating-box-bg">
           <div className="text-center">
-            <h2 className='mb-4 pb-3 border-bottom border-dark header-font'>Welcome, User's Name</h2>
-            <p>Here you'll find the favorited locations you saved and find more new adventures!</p>
+            <h2 className="mb-4 pb-3 border-bottom border-dark header-font">
+              Welcome, User's Name
+            </h2>
+            <p>
+              Here you'll find the favorited locations you saved and find more
+              new adventures!
+            </p>
           </div>
         </div>
         {/* SEARCH NEW PLACES */}
         <div className="col-5 p-4 m-2 shadow-lg floating-box-bg">
-          <div className='text-center'>
-            <h2 className='mb-4 pb-3 border-bottom border-dark header-font'>Your next adventure is waiting!</h2>
-            <h4 className='subHeader-font'>Please select the activity you're interested in</h4>
+          <div className="text-center">
+            <h2 className="mb-4 pb-3 border-bottom border-dark header-font">
+              Your next adventure is waiting!
+            </h2>
+            <h4 className="subHeader-font">
+              Please select the activity you're interested in
+            </h4>
           </div>
           <div className="d-flex justify-content-center align-items-center">
             <div className="m-2">
@@ -49,30 +60,54 @@ function Dashboard(props) {
       {/* USERS FAVORITE PLACES */}
       <div className="row d-flex row justify-content-center text-center">
         <div className="col-10 p-4 m-2 shadow-lg floating-box-bg">
-
           <div>
-            <h2 className="text-center mb-5 pb-3 border-bottom border-dark header-font">User Name's Favorite Places</h2>
+            <h2 className="text-center mb-5 pb-3 border-bottom border-dark header-font">
+              User Name's Favorite Places
+            </h2>
           </div>
           {/* Card Layout */}
           <div className="card col-4 p-3 dashboard-card-bg">
-            <img src={placeholderImg} className="card-img-top rounded" alt="..." />
+            <img
+              src={placeholderImg}
+              className="card-img-top rounded"
+              alt="..."
+            />
             <div className="card-body">
               <h5 className="card-title text-center">Placeholder Title</h5>
-              <p className="card-text text-center">Description -- only allow card to show first 50 characters of description</p>
+              <p className="card-text text-center">
+                Description -- only allow card to show first 50 characters of
+                description
+              </p>
             </div>
             <ul className="list-group list-group-flush text-center">
               <li className="list-group-item dashboard-card-bg">Category</li>
-              <li className="list-group-item dashboard-card-bg">Address/Location Name</li>
+              <li className="list-group-item dashboard-card-bg">
+                Address/Location Name
+              </li>
             </ul>
             {/* Add Comment */}
             <div className="mt-2">
-              <label htmlFor="validationTextarea" className="form-label">Comments</label>
-              <textarea className="form-control" id="add-comment" placeholder="Add A Comment" name="add-comment" type="text" rows="3" style={{ height: "100%" }}></textarea>
+              <label htmlFor="validationTextarea" className="form-label">
+                Comments
+              </label>
+              <textarea
+                className="form-control"
+                id="add-comment"
+                placeholder="Add A Comment"
+                name="add-comment"
+                type="text"
+                rows="3"
+                style={{ height: "100%" }}
+              ></textarea>
             </div>
-            < br />
+            <br />
             <div className="card-body text-center">
               {/* Open Modal */}
-              <Button variant="primary" className="dashboard-btn p-2 rounded" onClick={() => setModalShow(true)}>
+              <Button
+                variant="primary"
+                className="dashboard-btn p-2 rounded"
+                onClick={() => setModalShow(true)}
+              >
                 Show all comments
               </Button>
 
@@ -84,8 +119,8 @@ function Dashboard(props) {
           </div>
         </div>
       </div>
-      < br />
-      < br />
+      <br />
+      <br />
     </div>
   );
 }
