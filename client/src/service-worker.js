df2a92475a40169fc858ebdc58128614fa6beaff
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-27 21:33:48 -0400

    images resized, main.bundle still too large to cache

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -3,6 +3,8 @@ import { precacheAndRoute } from "workbox-precaching";
 
 clientsClaim();
 
+/* eslint-disable-next-line no-restricted-globals */
 self.skipWaiting();
 
+/* eslint-disable-next-line no-restricted-globals */
 precacheAndRoute(self.__WB_MANIFEST);
