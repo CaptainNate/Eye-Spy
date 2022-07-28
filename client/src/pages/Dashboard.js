7d27d533a5a3d4b8ff9644e799e70c4045ecef09
Fernando Almeida <nando.lalmeida@gmail.com>
2022-07-28 10:12:15 -0400

    fixed conflicts on aboutUs, LandingPage and Dashboard pages

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -17,7 +17,6 @@ function Dashboard(props) {
   return (
     // placeholder cards that will be filled with data from the backend
 
-<<<<<<< HEAD
     <div className="container p-5 mb-5">
                   <div className="">
                 <h1 className="mb-5 pb-3 border-bottom border-dark header-font text-center">Your Dashboard</h1>
@@ -34,30 +33,6 @@ function Dashboard(props) {
           <div className='text-center'>
             <h2 className='mb-4 pb-3 border-bottom border-dark header-font'>Your next adventure is waiting!</h2>
             <h4 className='subHeader-font'>Please select the activity you're interested in</h4>
-=======
-    <div className="container">
-      <div className="d-flex row justify-content-between text-center m-5">
-        <div className="col-6">
-          <div className="text-center m-5">
-            <h2 className="mb-4 pb-4 border-bottom border-dark">
-              Welcome, User's Name
-            </h2>
-            <p>
-              Here you'll find the favorited locations you saved and find more
-              new adventures!
-            </p>
-          </div>
-        </div>
-        {/* SEARCH NEW PLACES */}
-        <div className="col-6">
-          <div className="text-center m-5">
-            <h2 className="mb-4 pb-4 border-bottom border-dark">
-              Your next adventure is waiting!
-            </h2>
-            <h4 className="">
-              Please select the activity you're interested in
-            </h4>
->>>>>>> 75603cb9831e2465a6240c6185beb4d5822d3c26
           </div>
           <div className="d-flex justify-content-center align-items-center">
             <div className="m-2">
@@ -72,7 +47,6 @@ function Dashboard(props) {
         </div>
       </div>
       {/* USERS FAVORITE PLACES */}
-<<<<<<< HEAD
       <div className="row d-flex row justify-content-center text-center">
         <div className="col-10 p-4 m-2 shadow-lg floating-box-bg">
 
@@ -107,59 +81,6 @@ function Dashboard(props) {
                 onHide={() => setModalShow(false)}
               ></Modals>
             </div>
-=======
-      <div className="row d-flex justify-content-between mt-3">
-        <div>
-          <h2 className="text-center m-3 border-bottom border-dark p-3">
-            User Name's Favorite Places
-          </h2>
-        </div>
-        {/* Card Layout */}
-        <div className="card col-3 m-5">
-          <img
-            src={placeholderImg}
-            className="card-img-top mt-3 rounded"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title text-center">Placeholder Title</h5>
-            <p className="card-text text-center">
-              Description -- only allow card to show first 50 characters of
-              description
-            </p>
-          </div>
-          <ul className="list-group list-group-flush text-center">
-            <li className="list-group-item">Category</li>
-            <li className="list-group-item">Address/Location Name</li>
-          </ul>
-          {/* Add Comment */}
-          <div className="mt-4">
-            <label for="validationTextarea" className="form-label">
-              Comments
-            </label>
-            <textarea
-              className="form-control"
-              id="add-comment"
-              placeholder="Add A Comment"
-              name="add-comment"
-              type="text"
-              rows="3"
-              style={{ height: "100%" }}
-            ></textarea>
-          </div>
-          <div className="card-body text-center">
-            {/* Open Modal */}
-  
-            <Button variant="primary" onClick={() => setModalShow(true)}>
-              Show all comments
-            </Button>
-
-            <Modals
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-            ></Modals>
-
->>>>>>> 75603cb9831e2465a6240c6185beb4d5822d3c26
           </div>
         </div>
       </div>
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -15,44 +15,30 @@ function LandingPage() {
 
                 {/* webpage "introduction" */}
                 <div className="">
-<<<<<<< HEAD
-                    <h1 className="header-font">Welcome to Eye Spy...</h1>
-                    <br />
-                    <p className="">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas
-                        < br/>
-                        < br/>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas
-                    </p>
-=======
                     <h2 className="">Welcome to Eye Spy!</h2>
                     {/* <br /> */}
                     {/* <br /> */}
-                    <p class="fs-4 text"><em>Eye Spy Your Next Favorite Spot! </em>We are Your <em>Next Best Friend</em> for finding a new unique treasure. We'll help you find the next place you want to explore! </p>
-    <div class="container-fluid py-2 card mb-3 scrolling-wrapper">
-    {/* <h2 class="font-weight-light"></h2> */}
-    <div class="d-flex flex-row flex-nowrap row g-0">
-        {/* Static Reviews */}
-        <div class="card card-body"><p>Wow! I never thought I would find a site like this. I was able to find so many cool and unique places that were so close to home!</p>
-        <p> - Scott H.</p></div>
-        <div class="card card-body"><p>At first I thought it was such a weird webpage but it ended up being exactly what I was looking for! I can't even recommend it enough to anyone who likes being outdoors.</p>
-        <p>-Theodore M.</p></div>
-        <div class="card card-body"><p>I never thought anyone would think of an idea to find these unique gems. I'm always searching for these and a lot of them are just not what I'm looking for. This hits the nail on the head.</p>
-        <p>-Cleo P.</p></div>
-        {/* <div class="card card-body"><p>Eye Spy is a name that indeed matches. It's definitely got an eye for rare spots. I thought I was good, but they're even better.</p>
+                    <p className="fs-4 text"><em>Eye Spy Your Next Favorite Spot! </em>We are Your <em>Next Best Friend</em> for finding a new unique treasure. We'll help you find the next place you want to explore! </p>
+                    <div className="container-fluid py-2 card mb-3 scrolling-wrapper">
+                        {/* <h2 className="font-weight-light"></h2> */}
+                        <div className="d-flex flex-row flex-nowrap row g-0">
+                            {/* Static Reviews */}
+                            <div className="card card-body"><p>Wow! I never thought I would find a site like this. I was able to find so many cool and unique places that were so close to home!</p>
+                                <p> - Scott H.</p></div>
+                            <div className="card card-body"><p>At first I thought it was such a weird webpage but it ended up being exactly what I was looking for! I can't even recommend it enough to anyone who likes being outdoors.</p>
+                                <p>-Theodore M.</p></div>
+                            <div className="card card-body"><p>I never thought anyone would think of an idea to find these unique gems. I'm always searching for these and a lot of them are just not what I'm looking for. This hits the nail on the head.</p>
+                                <p>-Cleo P.</p></div>
+                            {/* <div className="card card-body"><p>Eye Spy is a name that indeed matches. It's definitely got an eye for rare spots. I thought I was good, but they're even better.</p>
         <p>-John E.</p></div> */}
-        {/* <div class="card card-body"><p>I was never one to look online to find places to explore, that weren't so busy, but here we are.I found this site and now I can't stop checking back!</p>
+                            {/* <div className="card card-body"><p>I was never one to look online to find places to explore, that weren't so busy, but here we are.I found this site and now I can't stop checking back!</p>
         <p>-Cillia T.</p></div> */}
-        <div class="card card-body"><p>What a refreshing site. It matches what I was looking for but just the webpage was so pleasant and nice to look at. I wish I could keep looking. I love the feature being able to add our own suggestions onto the website too! I feel so engaged.</p>
-        <p>-Lilliana L.</p></div>
-        
-    </div>
-</div>
+                            <div className="card card-body"><p>What a refreshing site. It matches what I was looking for but just the webpage was so pleasant and nice to look at. I wish I could keep looking. I love the feature being able to add our own suggestions onto the website too! I feel so engaged.</p>
+                                <p>-Lilliana L.</p></div>
 
->>>>>>> 75603cb9831e2465a6240c6185beb4d5822d3c26
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
@@ -67,17 +53,17 @@ function LandingPage() {
                     Signing up lets you add places to your favorites, submit places to be added to our webpage, allows you to comment and like places, and many more! Sign up now!
                 </p>
                 <div className="">
-                <a
-                className="all-btns p-2 rounded text-decoration-none"
-                href="/signup"
-                role="button"
-                >
-                Sign Up!
-                </a>
+                    <a
+                        className="all-btns p-2 rounded text-decoration-none"
+                        href="/signup"
+                        role="button"
+                    >
+                        Sign Up!
+                    </a>
                 </div>
             </div>
-            < br/>
-            < br/>
+            < br />
+            < br />
         </div>
     )
 }
