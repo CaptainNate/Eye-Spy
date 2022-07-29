2bbe314ab2c0866032f503e6e10bfda410eb8e32
maggiejoe <maggiejoseph94@gmail.com>
2022-07-28 21:57:15 -0400

    finished submit handler for add destination, committing to test in heroku

--- client/src/pages/AddDestination.js
+++ client/src/pages/AddDestination.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-// import { useMutations } from "@apollo/client"
-// import {} from '../utils/mutations'
+import { useMutation } from "@apollo/client"
+import { NEW_POST } from '../utils/mutations'
 
 // component imports
-import AddDestSearchBar from '../components/AddDestSearchBar';
+// import AddDestSearchBar from '../components/AddDestSearchBar';
 import SuccessModal from '../components/SuccessModal';
 
 // bootstrap imports
 import Button from "react-bootstrap/Button";
-// import Auth from "../utils/Auth";
+import Auth from "../utils/Auth";
 
 function AddDestination() {
     // useState variables for modals
@@ -16,39 +16,40 @@ function AddDestination() {
 
     // add a conditional statement so that all items in add-destination can be saved on click
     // create a formhandler?
-    // const [formState, setFormState] = useState({ });
-    // const [ saveDestination, { error }] = useMutation();
+    const [formState, setFormState] = useState({ img: "", post_title: "", category: "", post_text: "", location: "" });
+    const [saveDestination, { error }] = useMutation(NEW_POST);
 
-      // update state based on form input changes
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
+    //   update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    //     setFormState({
-    //         ...formState,
-    //         [name]: value,
-    //     });
-    //     console.log(formState);
-    // }
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+        console.log(formState);
+    }
 
     // submit form
-    // const handleFormSubmit = async (event) => {
-    //     event.preventDefault();
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    //     try {
-    //         const { data } = await saveDestination({
-    //             variable: { ...formState },
-    //         });
-            
-    //         Auth.login(data.login.token);
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
+        try {
+            const { data } = await saveDestination({
+                variable: { ...formState },
+            });
+
+            Auth.login(data.login.token);
+            console.log("success")
+        } catch (e) {
+            console.error(e);
+        }
 
         // clear from values
-        // setFormState({
+        setFormState({
 
-        // });
-    // }
+        });
+    }
 
     // Image Preview
     const loadFile = function (event) {
@@ -67,29 +68,40 @@ function AddDestination() {
                     <div>
                         <h1 className="text-center mb-5 pb-3 border-bottom border-dark header-font">Add Destination</h1>
                     </div>
-                    <form className="container d-flex flex-wrap justify-content-center">
+                    <form onSubmit={handleFormSubmit} className="container d-flex flex-wrap justify-content-center">
                         <div className="">
                             <div className="row justify-content-center">
                                 <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
                                     <div className="pb-2">
                                         <label className="form-label">Title</label>
-                                        <input id="destination-title" className="form-control" placeholder="Title" name="title" type="text"></input>
+                                        <input id="destination-title" className="form-control" placeholder="Title" name="title" type="text" onChange={handleChange} value={formState.post_title}></input>
                                     </div>
                                     <div className="">
                                         <label className="form-label">Address</label>
-                                        <input id="destination-address" className="form-control" placeholder="Address" name="address" type="text"></input>
+                                        <input id="destination-address" className="form-control" placeholder="Address" name="address" type="text" onChange={handleChange} value={formState.location}></input>
                                     </div>
                                 </div>
                                 <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
                                     <div className="">
                                         <label htmlFor="validationTextarea" className="form-label">Description</label>
-                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Please enter a brief description" name="description" type="text" rows="5" style={{ height: "100%" }} required></textarea>
+                                        <textarea className="form-control is-invalid" id="validationTextarea" placeholder="Please enter a brief description" name="description" type="text" rows="5" style={{ height: "100%" }} required onChange={handleChange} value={formState.post_text}></textarea>
                                     </div>
                                 </div>
                                 <div className="col-5 m-2 rounded shadow-lg floating-box-bg">
                                     <div>
                                         <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">Please Select A Category</h4>
-                                        <AddDestSearchBar />
+                                        <div>
+                                            <div className="text-center">
+                                                <select name="activity" className="category-btn p-2 rounded" placeholder='Select an outdoor activity' style={{ width: "100%" }}>
+                                                    <option value="" className="text-center" defaultValue={""}>Select an outdoor activity</option>
+                                                    <option value={formState.category} onChange={handleChange} id="search-bar-hiking" className="text-center">Hiking Trails</option>
+                                                    <option value={formState.category} onChange={handleChange} id="search-bar-camping" className="text-center">Camping</option>
+                                                    <option value={formState.category} onChange={handleChange} id="search-bar-biking" className="text-center">Mountain Biking</option>
+                                                    <option value={formState.category} onChange={handleChange} id="search-bar-swimming" className="text-center">Swimming</option>
+                                                    <option value={formState.category} onChange={handleChange} id="search-bar-water-sport" className="text-center">Water Sports</option>
+                                                </select>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 {/* upload image */}
@@ -100,7 +112,7 @@ function AddDestination() {
                                         <input className="text-center" type="file" id="img-file" name="file" accept="image/*" required onChange={loadFile} />
                                         <div className="col-12">
                                             {/* warning bc react wants an alt in the img element (alt is not necessary) */}
-                                            <img id="output" className="m-3 " style={{ height: "250px" }} alt="..." />
+                                            <img id="output" className="m-3 " style={{ height: "250px" }} alt="..." value={formState.img} onChange={handleChange} />
                                         </div>
                                     </div>
                                 </div>
@@ -116,71 +128,9 @@ function AddDestination() {
                             </div>
                         </div>
                     </form>
+                    {/* catch login error */}
+                    {error && <div>Something went wrong</div>}
                 </div>
-                <div className="col-5 p-4 m-2 rounded shadow-lg floating-box-bg">
-                    <div className="">
-                        <label htmlFor="validationTextarea" className="form-label">
-                            Description
-                        </label>
-                        <textarea
-                            className="form-control is-invalid"
-                            id="validationTextarea"
-                            placeholder="Please enter a brief description"
-                            name="description"
-                            type="text"
-                            rows="5"
-                            style={{ height: "100%" }}
-                            required
-                        ></textarea>
-                    </div>
-                </div>
-                <div className="col-5 m-2 rounded shadow-lg floating-box-bg">
-                    <div>
-                        <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">
-                            Please Select A Category
-                        </h4>
-                        <AddDestSearchBar />
-                    </div>
-                </div>
-                {/* upload image */}
-                <div className="col-5 m-2 text-center rounded shadow-lg floating-box-bg">
-                    <h4 className="text-center my-4 pb-3 border-bottom border-dark subHeader-font">
-                        Upload A Photo
-                    </h4>
-                    <div className="text-center">
-                        <label htmlFor="file" className="File"></label>
-                        <input
-                            className="text-center"
-                            type="file"
-                            id="img-file"
-                            name="file"
-                            accept="image/*"
-                            required
-                            onChange={loadFile}
-                        />
-                        <div className="col-12">
-                            {/* warning bc react wants an alt in the img element (alt is not necessary) */}
-                            <img
-                                id="output"
-                                className="m-3 "
-                                style={{ height: "250px" }}
-                                alt="..."
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                {/* Open Modal */}
-                <Button
-                    className="all-btns m-2 p-2 rounded text-center col-4"
-                    onClick={() => setModalShow(true)}
-                >
-                    Add Destination
-                </Button>
-                <SuccessModal
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                ></SuccessModal>
             </div>
         </div>
     );
