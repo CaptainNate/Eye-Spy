09d7f2129d05f7d22f93b74367675a3416667e1e
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-08-03 12:57:52 -0400

    Added places to show numbers

--- client/src/components/LikeDislikeButton/index.js
+++ client/src/components/LikeDislikeButton/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
+
+
 function LikeDislikeButton() {
   return (
     <div>
       <Container>
+        <h1>{numberLike}</h1>
       <Button onClick={} id="" className="all-btns">Like</Button>
       <Button onClick={} id="" className="all-btns">Dislike</Button>
+      <h1>{numberDislike}</h1>
       </Container>
     </div>
   );
