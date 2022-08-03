56f8d528682c790dd4a1cfc89c065695b4fa0526
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-08-03 12:25:02 -0400

    create basic container and import

--- client/src/components/LikeDislikeButton/index.js
+++ client/src/components/LikeDislikeButton/index.js
@@ -1,2 +1,13 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+
+function LikeDislikeButton() {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
+}
+
+export default LikeDislikeButton;
