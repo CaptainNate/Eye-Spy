c53455035591c59cde1be51bb35a1057e37d0d39
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-08-05 00:22:37 -0400

    Update scope of variables

--- client/src/components/LikeDislikeButton/index.js
+++ client/src/components/LikeDislikeButton/index.js
@@ -4,30 +4,29 @@ import Button from "react-bootstrap/Button";
 import { useState } from "react";
 
 function LikeDislikeButton() {
+  const [numberLike, setNumberLike] = useState(0);
 
-    const likes() {
-        const [numberLike, setNumberLike] = useState(0);
-    }        
-    
-    const increase = () => {
-            setNumberLike((currentNumber) => currentNumber + 1);
-        }
-    
-    const dislikes = () => {
-        const [numberDislike, setNumberDislike] = useState(0);
-    }
+  const increase = () => {
+    setNumberLike((currentNumber) => currentNumber + 1);
+  };
 
-    const decrease = () => {
-        setNumberDislike((currentNumber) => currentNumber - 1);
-    }
+  const [numberDislike, setNumberDislike] = useState(0);
+
+  const decrease = () => {
+    setNumberDislike((currentNumber) => currentNumber - 1);
+  };
 
   return (
     <div>
       <Container>
         <h1>{numberLike}</h1>
-      <Button onClick={increase} id="" className="all-btns">Like</Button>
-      <Button onClick={decrease} id="" className="all-btns">Dislike</Button>
-      <h1>{numberDislike}</h1>
+        <Button onClick={increase} id="" className="all-btns">
+          Like
+        </Button>
+        <Button onClick={decrease} id="" className="all-btns">
+          Dislike
+        </Button>
+        <h1>{numberDislike}</h1>
       </Container>
     </div>
   );
